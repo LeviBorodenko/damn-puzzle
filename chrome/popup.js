f854b76e26e637d6abe3fee397c781158a92bde7
Levi Borodenko <levi.borodenko@gmail.com>
2020-06-20 12:26:28 +0200

    Fully functioning version.

--- chrome/popup.js
+++ chrome/popup.js
@@ -14,15 +14,16 @@ async function fetch_vote(url) {
 }
 
 
+
+
 async function handleButtonPress() {
     chrome.tabs.query({active: true, lastFocusedWindow: true}, async tabs => {
         let url = tabs[0].url;
         // use `url` here inside the callback because it's asynchronous!
         let vote = await fetch_vote(url)
-        alert(vote)
+        let button = document.getElementById('button');
+        button.innerHTML = `<span id="vote">${vote}</span>`
     });
 }
-
-let button = document.getElementById('button');
 
 button.onclick = handleButtonPress
