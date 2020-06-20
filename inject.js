42428dc37c46f0cd863848da445ffe03abbd9382
Levi Borodenko <levi.borodenko@gmail.com>
2020-06-20 03:17:24 +0200

    Switched over to an API query to obtain score
    - this prevents issues with having finding the score on the next puzzle

--- inject.js
+++
@@ -1,8 +0,0 @@
-
-window.myScript = document.createElement("script");
-
-window.myScript.innerHTML = `
-    alert('This puzzle has a rating of ' + window.lichess.puzzle.data.puzzle.vote);
-    `;
-
-document.body.appendChild(window.myScript);
--- popup.js
+++ popup.js
@@ -3,8 +3,24 @@ function sleep(ms) {
       return new Promise(resolve => setTimeout(resolve, ms));
    }
 
+async function fetch_vote(url) {
+    let response = await fetch(url)
+    let text = await response.text()
+    let index = text.search(`"vote":`)
+    let vote_string = text.slice(index + 7, index + 13)
+
+    let result = /.\d+/.exec(vote_string)
+    return result[0]
+}
+
+
 async function handleButtonPress() {
-    chrome.tabs.executeScript({file:"inject.js"})
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, async tabs => {
+        let url = tabs[0].url;
+        // use `url` here inside the callback because it's asynchronous!
+        let vote = await fetch_vote(url)
+        alert(vote)
+    });
 }
 
 let button = document.getElementById('button');
